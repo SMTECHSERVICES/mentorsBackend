3cd555fc5b44cf9aec38de6a3aeea14f9cd415a9
Er.Abhishek Mishra <abhishekmishra59@gmail.com>
2025-07-15 16:21:14 +0530

    chechig cookie

--- routes/mentee.js
+++ routes/mentee.js
@@ -70,6 +70,7 @@ console.log("Cloudinary ENV:", {
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         path: "/",
+        domain: process.env.NODE_ENV === 'production' ? '.mentors.ind.in' : undefined,
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
@@ -122,6 +123,7 @@ router.post('/login',async(req,res)=>{
            secure: process.env.NODE_ENV === "production",
            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
            path: "/",
+           domain: process.env.NODE_ENV === 'production' ? '.mentors.ind.in' : undefined,
            maxAge: 7 * 24 * 60 * 60 * 1000,
          });
    
